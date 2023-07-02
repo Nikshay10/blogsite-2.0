58753ac27d3983dad7ecff343e46356a22991162
Nikshay10 <nikshayreddygattu123@gmail.com>
2023-07-02 16:17:33 +0530

    designing ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,15 @@ import React from 'react'
 import './Header.css'
 function Header() {
   return (
-    <div className='header'>
-        <h1>Blogsite</h1>
-    </div>
+    <nav className='header'>
+      <div className='logo'>
+        <p>Nikshay's Blog</p>
+      </div>
+      <ul>
+        <li><a href="#" className='login'>Login</a></li>
+        <li><a href="#" className='register'>Register</a></li>
+      </ul>
+    </nav>
   )
 }
 
--- src/queries/postQuery.js
+++ src/queries/postQuery.js
@@ -3,30 +3,26 @@ import { gql } from "@apollo/client";
 export const GET_POSTS=gql`
 query GetPosts($first: Int,$after: String) {
     postsConnection(first: $first, after:$after){
-      edges {
-        node {
-          id
-          title
-
-          
-  datePublished,
-  slug,
-  content{
-    html
-  }
-  author{
-    name,
-    avatar{
-      url
-    }
-  }
-  coverPhoto{
-      url
-  }
+        edges {
+            node {
+                id
+                title
+                datePublished,
+                slug,
+                content{
+                    html
+                }
+                author{
+                    name,
+                    avatar{
+                        url
+                    }
+                }
+                coverPhoto{
+                    url
+                }
+            }
+            cursor
         }
-        cursor
-      }
-   }
-     
-   }
-`
+    } 
+}`
