e362f6521893a03fdd3ff8cd396cde9bbcaabec7
Nikshay10 <nikshayreddygattu123@gmail.com>
2023-07-06 20:12:50 +0530

    sorted the files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import PostList from './components/PostList';
-import Sidebar from './components/Sidebar';
-import Body from './components/Body';
+import Body from './components/Home/Body';
 
 const GQL_URI='https://api-ap-south-1.hygraph.com/v2/clja2cxwd24l001t2308g4tju/master';
 
@@ -16,9 +12,9 @@ const apolloClient=new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
-    <div className="App">
-      <Body/>
-    </div>
+      <div className="App">
+        <Body/>
+      </div>
     </ApolloProvider>
   );
 }
