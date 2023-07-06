90cfc48b72607596b790038486ffa5d60d2cb2ae
Nikshay10 <114459048+Nikshay10@users.noreply.github.com>
2023-07-06 20:26:32 +0530

    3 files modified

--- src/components/Home/PostCard.js
+++ src/components/Home/PostCard.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import './PostCard.css'
-import {browserHistory, Link,Route,Routes } from 'react-router-dom'
+import React from 'react';
+import './PostCard.css';
+import { browserHistory, Link, Route, Routes } from 'react-router-dom';
 
-const PostCard=({id,title,author,content,datePublished,coverPhoto,slug})=> {
+const PostCard = ({
+  id,
+  title,
+  author,
+  content,
+  datePublished,
+  coverPhoto,
+  slug,
+}) => {
   return (
-    <div class="post">
-      <div class="post-header">
-        <img src={author.avatar.url} class="avatar"/>
-        <div class="user-info">
-          <h2 class="user-name">{author.name}</h2>
-          <p class="post-date">{datePublished}</p>
+    <div className="post">
+      <div className="post-header">
+        <img src={author.avatar.url} className="avatar" />
+        <div className="user-info">
+          <h2 className="user-name">{author.name}</h2>
+          <p className="post-date">{datePublished}</p>
         </div>
       </div>
-      <h1>{title}</h1><Link to={"/Posts/"+slug}>
-      <img src={coverPhoto.url} class="cover-photo"/>
+      <h1>{title}</h1>
+      <Link to={'/Posts/' + slug}>
+        <img src={coverPhoto.url} className="cover-photo" />
       </Link>
       <div
-          className='post-content'
-          dangerouslySetInnerHTML={{ __html: content.html }}
-        />
+        classNameName="post-content"
+        dangerouslySetInnerHTML={{ __html: content.html }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
